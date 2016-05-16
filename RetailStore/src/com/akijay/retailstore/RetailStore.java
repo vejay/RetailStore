f77d2c4d8e96804b7a8ebbf47990d98801cec346
vijay <vijay@localhost>
2016-05-16 18:33:48 -0500

    Initial commit

---
+++ RetailStore/src/com/akijay/retailstore/RetailStore.java
@@ -0,0 +1,10 @@
+package com.akijay.retailstore;
+
+public class RetailStore 
+{
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, and Welcome to my RetailStore");
+	}
+}
