ae86013a8aadb731f92c34a28c889bd27289b141
vijay <vijay@localhost>
2016-05-16 19:36:36 -0500

    Adding initial example springboot class.

---
+++ RetailStore/src/main/java/com/akijay/retailstore/Example.java
@@ -0,0 +1,22 @@
+package com.akijay.retailstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class Example 
+{
+	@RequestMapping("/")
+	String home()
+	{
+		return "Hello World21!";
+	}
+	
+	public static void main(String[] args) throws Exception
+	{
+		SpringApplication.run(Example.class, args);
+	}
+}
