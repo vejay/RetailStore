909defc5c491eb48731fdcf5eaef3c3c6147535e
vijay <vijay@localhost>
2016-05-16 19:14:21 -0500

    Adding intial directory structure

---
+++ RetailStore/src/main/java/com/akijay/retailstore/RetailStore.java
@@ -0,0 +1,9 @@
+package com.akijay.retailstore;
+
+public class RetailStore 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("This is an initial version of RetailStore");
+	}
+}
