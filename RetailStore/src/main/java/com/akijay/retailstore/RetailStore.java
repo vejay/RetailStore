7e701d3a92f1a86e4b8e20d0f36f34bb3b214c91
Vijay Vasanthapuram <vijay@Vijays-MacBook-Pro.local>
2016-10-16 11:07:08 -0500

    Revert "Sample neo4j test code"
    This reverts commit 8e5275d544bda4f6d2dd3ac29d094ba4619fb50f.

--- RetailStore/src/main/java/com/akijay/retailstore/RetailStore.java
+++ RetailStore/src/main/java/com/akijay/retailstore/RetailStore.java
@@ -1,61 +1,9 @@
 package com.akijay.retailstore;
 
-import java.io.File;
-
-import org.neo4j.graphdb.GraphDatabaseService;
-import org.neo4j.graphdb.Label;
-import org.neo4j.graphdb.Node;
-import org.neo4j.graphdb.Relationship;
-import org.neo4j.graphdb.RelationshipType;
-import org.neo4j.graphdb.Transaction;
-import org.neo4j.graphdb.factory.GraphDatabaseFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@EnableAutoConfiguration
 public class RetailStore 
 {
-	@RequestMapping("/")
-	String home()
+	public static void main(String[] args)
 	{
-		return "Hello World21!";
-	}
-	
-	public enum Tutorial implements Label 
-	{
-		JAVA,SCALA,SQL,NEO4J;
-	}
-	
-	public enum TutorialRelationships implements RelationshipType
-	{
-		JVM_LANUGAGES, NON_JVM_LANGUAGES;
-	}
-	
-	public static void main(String[] args) throws Exception
-	{
-		SpringApplication.run(RetailStore.class, args);
-		GraphDatabaseFactory dbFactory = new GraphDatabaseFactory();
-		GraphDatabaseService db = dbFactory.newEmbeddedDatabase(new File("/Users/vijay/testne04jDB"));
-		
-		try (Transaction tx = db.beginTx()) 
-		{
-			// Perform DB operations
-			Node javaNode = db.createNode(Tutorial.JAVA);
-			javaNode.setProperty("id", "001");
-			javaNode.setProperty("name", "Learn Java");
-			
-			
-			Node scalaNode = db.createNode(Tutorial.SCALA);
-			scalaNode.setProperty("id", "002");
-			scalaNode.setProperty("name", "Learn Scala");
-			
-			Relationship relationship = javaNode.createRelationshipTo(scalaNode, TutorialRelationships.JVM_LANUGAGES);
-			relationship.setProperty("oops", "yes");
-			
-			tx.success();
-		}
+		System.out.println("This is an initial version of RetailStore");
 	}
 }
